6ce1274c5d73d5701e7ada8360986e4d774f4ceb
Fernanda Sampaio <fernanda@MacBook-Air-de-Fernanda.local>
2019-12-27 17:49:35 -0300

    landingpagereact

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const webpack = require('webpack');
+
 module.exports = {
     context: __dirname,
     entry: './src/index.js',
@@ -31,7 +33,13 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: path.resolve(__dirname, 'public/index.html'),
             filename: './index.html'
-        })
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.ProvidePlugin({
+            '$': "jquery",
+            'jQuery': "jquery",
+            'Popper': 'popper.js'
+        }),
     ]
 };
 
